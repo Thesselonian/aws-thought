70d81c9bc476651b5370fc7d948540985af32763
Eric <ericyoung111@gmail.com>
2022-03-12 10:21:02 -0700

    feature/userthoughts-table started making the create thoughts table in AWS

---
+++ server/db/createThoughtsTable.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region: 'us-east-2',
+});
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const params = {
+    TableName: 'Thoughts',
+    KeySchema: [
+        { AttributeName: 'username', KeyType: 'HASH' }, //Partition key
+        { AttributeName: 'createdAt', KeyType: 'RANGE' }, // Sort key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'username', AttributeType: 'S' },
+        { AttributeName: 'createdAt', AttributeType: 'N' },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10,
+    },
+};
